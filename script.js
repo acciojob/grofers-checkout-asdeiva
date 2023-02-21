d7034ee88034d1782be53cd257239b709762a405
DEIVA SIVAM <115927155+asdeiva@users.noreply.github.com>
2023-02-21 12:17:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let price = document.querySelectorAll(".prices") 
+	let sum = 0
+	for (let i of price) {
+		sum+=price[i]
+	}
+	
+	let tot = `<tr>
+      <td class="item">Total</td>
+      <td class="price">${sum}</td>
+    </tr>`;
+	const total = document.createElement(tot);
+	document.table.appendChild(total);
+	
   
 };
 
